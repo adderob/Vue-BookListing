d41ce694b37152181b4c475d531bf7524265cbe0
Rob Nichols <rob.nichols@amwins.com>
2022-08-11 10:02:37 -0400

    Push 4

---
+++ src/components/BookItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li v-for="book in books">{{ book.title }} : {{ book.author }}</li>
+</template>
+<script>
+export default{
+  name: "BookItem",
+  data () {
+    return {
+      books:
+      [
+        {book: "book"}
+      ]
+    }
+  }
+}
+</script>
+<style>
+li{
+  display: block;
+  margin: 0 10px;
+}
+</style>
